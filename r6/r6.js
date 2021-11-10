77cda14b368631dfd303483467aff81026c6f84f
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2021-11-11 01:17:35 +0800

    Update r6.js

--- r6/r6.js
+++ r6/r6.js
@@ -1,106 +1,72 @@
-var embed = require('./embed.js');
-var record = require('./r6_record');
-var base = require('./base.json')
+const embed = require('./embed.js');
+const record = require('./r6_record');
+const base = require('./base.json');
 
 
 module.exports = {
-    R6_type: function (type, r6name, tracker, operators) {
+    /* [] 71-75
+    *  71=> 間隔一個賽季以上或從沒打過RANK的人
+    *  75=> normal rank & casual 
+    */
 
-        console.log('type : ' + type)
-        let operators_check = checking(type);
-        console.log('operators : ' + operators_check)
-        /*
-        for (i = 0; i <= tracker.length; ++i) {
-            console.log(i + ' = [' + tracker[i] + ']')
-        }*/
-
-        /* [] 71-75
-         *  71=> 間隔一個賽季以上或從沒打過RANK的人
-         *  75=> normal rank & casual 
-         */
-        if (tracker[42] !== undefined && tracker[44] !== undefined) { //檢查搜尋到的玩家是否正確(存在)
-            console.log('done')
-
-            if (type === 'RANK' && r6name !== undefined) {
-                console.log('RANK')//season RANK
-
-                if (tracker.length == 71) {
-                    console.log('71')
-                    return embed.R6_Ranked_Embed(record.header, record.user, record.url, record.HIDE_RANK_win_percent, record.HIDE_RANK_win, record.HIDE_RANK_loss, record.HIDE_RANK_kd, record.HIDE_RANK_kill, record.HIDE_RANK_death, record.HIDE_RANK_killMatch, record.HIDE_RANK_rank, record.HIDE_RANK_mmr, record.HIDE_RANK_rank_img)
-                }
-                else {
-                    console.log('75')
-                    //header, user, url, win_percent, win, loss, kd, kill, death, killMatch, rank, mmr, rank_img
-                    return embed.R6_Ranked_Embed(record.header, record.user, record.url, record.RANK_win_percent, record.RANK_win, record.RANK_loss, record.RANK_kd, record.RANK_kill, record.RANK_death, record.RANK_killMatch, record.RANK_rank, record.RANK_mmr, record.RANK_rank_img)
-                }
-            }
-            else if (type === 'CASUAL' && r6name !== undefined) {
-                console.log('CASUAL')//casual
-
-                if (tracker.length == 71) {
-                    console.log('71')
-                    return embed.R6_Casual_Embed(record.header, record.user, record.url, record.HIDE_CASUAL_timePlayed, record.HIDE_CASUAL_win_percent, record.HIDE_CASUAL_win, record.HIDE_CASUAL_loss, record.HIDE_CASUAL_kd, record.HIDE_CASUAL_kill, record.HIDE_CASUAL_death, record.HIDE_CASUAL_killMatch, record.HIDE_CASUAL_rank, record.HIDE_CASUAL_mmr, record.HIDE_CASUAL_rank_img)
-                }
-                else {
-                    console.log('75')
-                    //header, user, url, timePlayed, win_percent, win, loss, kd, kill, death, killMatch, rank, mmr, rank_img
-                    return embed.R6_Casual_Embed(record.header, record.user, record.url, record.CASUAL_timePlayed, record.CASUAL_win_percent, record.CASUAL_win, record.CASUAL_loss, record.CASUAL_kd, record.CASUAL_kill, record.CASUAL_death, record.CASUAL_killMatch, record.CASUAL_rank, record.CASUAL_mmr, record.CASUAL_rank_img)
-                }
-            }
-            else if (type === undefined) {
-                console.log('GENERAL')//general
-
-                //header, user, url, timePlayed, win_percent, win, loss, kd, death, handShot, handShots, meleeKills, blindKills 
-                return embed.R6_General_Embed(record.header, record.user, record.url, record.GENERAL_timePlayed, record.GENERAL_win_percent, record.GENERAL_win, record.GENERAL_loss, record.GENERAL_kd, record.GENERAL_death, record.GENERAL_handShot, record.GENERAL_handShots, record.GENERAL_meleeKills, record.GENERAL_blindKills)
-            }
-/*
-            else if (r6name === 'HELP' || r6name === 'help') {//不是則檢測是否為 +r6 help
-                console.log('HELP')
-                return embed.R6_help()
-            }*/
-            else if (operators_check === true) {
-                console.log('OPERATORS')
-
-                for (var i = 1; i < operators.length; ++i) {
-                    console.log('--' + operators[i])
-                    if (operators[i][3] == 'NØKK'){
-                        operators[i][3] = 'NAKK';}
-                    if (type == operators[i][3]) {
-                        console.log('++' + operators[i])
-                        for (j = 0; j < 20; ++j) {
-                            console.log(`[${j}] = ${operators[i][j]}`)
-                        }
-                        //console.log('++++++' + operators[i])
-                        var header = `https://trackercdn.com/cdn/r6.tracker.network/operators/badges/${type.toLowerCase()}.png`
-                                                      //header,        user,        url,        Operator ,    Time Played,           Kills,          Deaths,             K/D,              Wins,           Losses,            Win %,        Headshot %,           DBNOs,               XP,      Melee Kills,   Operator Stat,
-                        return embed.R6_Operators_Embed(header, record.user, record.url, operators[i][3], operators[i][6], operators[i][7], operators[i][8], operators[i][9], operators[i][10], operators[i][11], operators[i][12], operators[i][13], operators[i][14], operators[i][15], operators[i][16], operators[i][18])
-                    }
-                }
-
-                console.log('-------- operators --------' + operators[1][0])
-                return embed.R6_Not_Found()//operators[1][0]
-            }
-            else {
-                console.log('Not Found')
-                return embed.R6_Not_Found()
-            }
+    Rank: function (tracker) {
+        if (tracker.length == 71) {
+            console.log('71')
+            return embed.R6_Ranked_Embed(record.header, record.user, record.url, record.HIDE_RANK_win_percent, record.HIDE_RANK_win, record.HIDE_RANK_loss, record.HIDE_RANK_kd, record.HIDE_RANK_kill, record.HIDE_RANK_death, record.HIDE_RANK_killMatch, record.HIDE_RANK_rank, record.HIDE_RANK_mmr, record.HIDE_RANK_rank_img)
+        }
+        else {
+            console.log('75')
+            //header, user, url, win_percent, win, loss, kd, kill, death, killMatch, rank, mmr, rank_img
+            return embed.R6_Ranked_Embed(record.header, record.user, record.url, record.RANK_win_percent, record.RANK_win, record.RANK_loss, record.RANK_kd, record.RANK_kill, record.RANK_death, record.RANK_killMatch, record.RANK_rank, record.RANK_mmr, record.RANK_rank_img)
         }
     },
 
-    filterArray: function (clearArray) {
-        let index = -1,
-            arrayLength = clearArray ? clearArray.length : 0,
-            resIndex = -1,
-            result = [];
+    Casual: function (tracker) {
+        if (tracker.length == 71) {
+            console.log('71')
+            return embed.R6_Casual_Embed(record.header, record.user, record.url, record.HIDE_CASUAL_timePlayed, record.HIDE_CASUAL_win_percent, record.HIDE_CASUAL_win, record.HIDE_CASUAL_loss, record.HIDE_CASUAL_kd, record.HIDE_CASUAL_kill, record.HIDE_CASUAL_death, record.HIDE_CASUAL_killMatch, record.HIDE_CASUAL_rank, record.HIDE_CASUAL_mmr, record.HIDE_CASUAL_rank_img)
+        }
+        else {
+            console.log('75')
+            //header, user, url, timePlayed, win_percent, win, loss, kd, kill, death, killMatch, rank, mmr, rank_img
+            return embed.R6_Casual_Embed(record.header, record.user, record.url, record.CASUAL_timePlayed, record.CASUAL_win_percent, record.CASUAL_win, record.CASUAL_loss, record.CASUAL_kd, record.CASUAL_kill, record.CASUAL_death, record.CASUAL_killMatch, record.CASUAL_rank, record.CASUAL_mmr, record.CASUAL_rank_img)
+        }
+    },
 
-        while (++index < arrayLength) {
-            let value = clearArray[index];
-            if (value != null && value !== '' && value !== undefined && value !== false && value !== 0 && value != ',') {
-                result[++resIndex] = value;
+    General: function () {
+        //header, user, url, timePlayed, win_percent, win, loss, kd, death, handShot, handShots, meleeKills, blindKills 
+        return embed.R6_General_Embed(record.header, record.user, record.url, record.GENERAL_timePlayed, record.GENERAL_win_percent, record.GENERAL_win, record.GENERAL_loss, record.GENERAL_kd, record.GENERAL_death, record.GENERAL_handShot, record.GENERAL_handShots, record.GENERAL_meleeKills, record.GENERAL_blindKills)
+
+    },
+
+    Operators: function (operators, type) {
+        for (var i = 1; i < operators.length; ++i) {
+            console.log('--' + operators[i])
+            if (operators[i][3] == 'NØKK') {
+                operators[i][3] = 'NAKK';
+            }
+            if (operators[i][3] == 'JÄGER') {
+                operators[i][3] = 'JAGER';
+            }
+            if (operators[i][3] == 'CAPITÃO') {
+                operators[i][3] = 'CAPITAO';
+            }
+            if (type == operators[i][3]) {
+                console.log('++' + operators[i])
+                for (j = 0; j < 20; ++j) {
+                    console.log(`[${j}] = ${operators[i][j]}`)
+                }
+                //console.log('++++++' + operators[i])
+                var header = `https://trackercdn.com/cdn/r6.tracker.network/operators/badges/${type.toLowerCase()}.png`
+                //header,        user,        url,        Operator ,    Time Played,           Kills,          Deaths,             K/D,              Wins,           Losses,            Win %,        Headshot %,           DBNOs,               XP,      Melee Kills,   Operator Stat,
+                return embed.R6_Operators_Embed(header, record.user, record.url, operators[i][3], operators[i][6], operators[i][7], operators[i][8], operators[i][9], operators[i][10], operators[i][11], operators[i][12], operators[i][13], operators[i][14], operators[i][15], operators[i][16], operators[i][18])
             }
         }
-        return result
+
+        //console.log('-------- operators --------' + operators[1][0])
+        return embed.R6_Not_Found()//operators[1][0]
     },
+
 
     R6_record: function (header, r6name, url, tracker) {
 
@@ -169,7 +135,33 @@ module.exports = {
         record.HIDE_RANK_mmr = String(tracker[57]);
         record.HIDE_RANK_rank_img = `https://cdn.tabstats.com/tabstats/r6/ranks/?rank=${RankImage(tracker[55])}&champ=0`;
     },
+
+
+    FilterArray: function (clearArray) {
+        let index = -1,
+            arrayLength = clearArray ? clearArray.length : 0,
+            resIndex = -1,
+            result = [];
+
+        while (++index < arrayLength) {
+            let value = clearArray[index];
+            if (value != null && value !== '' && value !== undefined && value !== false && value !== 0 && value != ',') {
+                result[++resIndex] = value;
+            }
+        }
+        return result;
+    },
+
+    OperatorCheck: function (type) {
+        for (var i = 0; i < base.operators.length; ++i) {
+            //console.log(type, '=', base.operators[i])
+            if (type == base.operators[i]) {
+                return true
+            }
+        } return false
+    }
 }
+
 
 
 
@@ -211,11 +203,4 @@ function RankImage(Img) {
 }
 
 
-function checking(type) {
-    for (var i = 0; i < base.operators.length; ++i) {
-        console.log(type, '=', base.operators[i])
-        if (type == base.operators[i]) {
-            return true
-        }
-    } return false
-}
+
