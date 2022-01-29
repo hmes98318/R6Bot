457b3ff351abe4427f75eb23b651fc606653ae74
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-01-29 19:18:22 +0800

    Update operator.js

--- commands/operator.js
+++ commands/operator.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+const R6 = require('../r6/r6.js');
+const tracker = require('../r6/tracker.js');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('r6_operators')
@@ -8,9 +11,9 @@ module.exports = {
             option.setName('platform')
                 .setDescription('game platform')
                 .setRequired(true)
-                .addChoice('pc', 'PC')
-                .addChoice('psn', 'PSN')
-                .addChoice('xbox', 'XBOX'))
+                .addChoice('pc', 'pc')
+                .addChoice('psn', 'psn')
+                .addChoice('xbox', 'xbox'))
         .addStringOption(option => option.setName('name')
             .setDescription('player name')
             .setRequired(true))
@@ -19,5 +22,21 @@ module.exports = {
             .setRequired(true))
     ,
     async execute(interaction) {
+        await interaction.deferReply();
+
+        let platform = interaction.options.getString("platform");
+        let name = interaction.options.getString("name");
+        let operator = interaction.options.getString("operator");
+
+        let profile = [];
+        let operators = [];
+        let header;
+        let url_profile = `https://r6.tracker.network/profile/${platform}/${name}`;
+        let url_operators = `https://r6.tracker.network/profile/${platform}/${name}/operators`;
+
+        operators = await tracker.Operators(operators, url_operators);
+        R6.R6_record(header, name, url_profile, profile);
+
+        await interaction.editReply({ embeds: [R6.Operators(operators, operator.toUpperCase())] });
     }
 };
