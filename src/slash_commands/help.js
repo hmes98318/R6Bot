fbf64b2b90f0f77b7fd09eec961596be9aaf4e37
hmes98318 <s0925677069@gmail.com>
2022-04-23 02:26:16 +0800

    move files location

---
+++ src/slash_commands/help.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const embed = require('../embeds/embeds.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('r6 help')
+    ,
+    async execute(interaction) {
+        interaction.reply({ embeds: [embed.R6_help()] });
+    }
+};
